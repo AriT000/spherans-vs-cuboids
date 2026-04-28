fa3c18f1df8149d0e51ecbe575176c47b4d97bae
filivilla <filivilla78@gmail.com>
2026-04-27 23:18:26 -0700

    added assets

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,78 @@
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+public class PauseMenu : MonoBehaviour
+{
+    private PlayerControls playerControls;
+    private InputAction menu;
+
+    [SerializeField] private GameObject pauseUI;
+    [SerializeField] private bool isPaused;
+
+
+    void Awake()
+    {
+        playerControls = new PlayerControls();
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void OnEnable()
+    {
+        menu = playerControls.Menu.Escape;
+        menu.Enable();
+
+        menu.performed += Pause;
+    
+    }
+
+    private void OnDisable()
+    {
+        menu.Disable();
+    }
+
+    void Pause(InputAction.CallbackContext context)
+    {
+        isPaused = !isPaused;
+        if (isPaused)
+        {
+            ActivateMenu();
+        }
+        else
+        {
+            DeactivateMenu();
+        }
+    }
+
+    void ActivateMenu()
+    {
+        Time.timeScale = 0;
+        AudioListener.pause = true;
+        pauseUI.SetActive(true);
+    }
+
+    public void DeactivateMenu()
+    {
+        Time.timeScale = 1;
+        AudioListener.pause = false;
+        pauseUI.SetActive(false);
+        isPaused = false;
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1;
+    }
+}
