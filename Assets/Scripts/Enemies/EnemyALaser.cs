0303a36d92cdd9b5e060f97cb31750ed742fea5f
Nathan <53132424+Ree-Nat@users.noreply.github.com>
2026-04-29 11:44:51 -0700

    create Enemy A Prefab

---
+++ Assets/Scripts/Enemies/EnemyALaser.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class EnemyALaser : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem particleSystem;
+    [SerializeField] private Transform player;
+    [SerializeField] private int bulletCount = 1;
+
+    [SerializeField] private float fireRate = 1.2f;
+
+    private float timer;
+
+    void Awake()
+    {
+        particleSystem = GetComponent<ParticleSystem>();
+    }
+
+    void Update()
+    {
+        if (player != null)
+        {
+            Vector2 dir = player.position - transform.position;
+            float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0, 0, angle);
+        }
+
+        timer += Time.deltaTime;
+
+        if (timer >= fireRate)
+        {
+            timer = 0f;
+            particleSystem.Emit(bulletCount);
+        }
+    }
+}
