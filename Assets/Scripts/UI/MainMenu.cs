ddd7856f19c8b8fd9562a6ce2aafba14f838ed0e
filivilla <filiberto.villa2532@my.riohondo.edu>
2026-05-04 21:12:39 -0700

    fixed mainmenu

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -11,7 +11,7 @@ public class MainMenu : MonoBehaviour
 
     public void PlayGame(string scene_name)
     {
-        SceneManager.LoadScene(scene_name);
+        SceneManager.LoadScene("2roundsBoss");
     }
 
     public void QuitGame()
